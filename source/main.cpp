683ca5aa7665712129dcbe59e524f59c685d473e
Maxime Bouchet <maxime.bouchet@epitech.eu>
2020-01-18 16:25:12 +0100

    store file commands in a vector

--- source/main.cpp
+++ source/main.cpp
@@ -9,11 +9,14 @@
 #include "Factory.hh"
 #include "IOperand.hh"
 #include "AbstractVmException.hh"
+#include <fstream>
+#include <iostream>
 
 using namespace std;
 
-int main ()
+int main (int ac, char **av)
 {
+    /*
   std::vector<std::string> str;
   str.push_back("one");
   str.push_back("two");
@@ -26,4 +29,24 @@ int main ()
     std::cout << str.at(i) << std::endl;
   }
   std::cout << str.size() << std::endl;
+   
+
+  if (ac == 1)
+    cout << "Enter a command" << endl;
+  else if (ac == 2)
+    cout << "Read the file" << endl;
+  else
+    cout << "Errot too much arguments"  << endl; */
+  
+  std::vector<std::string>commands;
+  std::ifstream file;
+  file.open("test.avm");
+  std::string line;
+  if (file.is_open()) {
+    while (getline(file, line))
+      commands.push_back(line);
+  } else
+    std::cout << "no files match" << std::endl;
+  file.close();
+  return 0;
 }
