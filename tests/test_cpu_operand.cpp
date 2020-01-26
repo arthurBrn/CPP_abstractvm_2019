357cabb82ff3245e4058a19860f5ea900b1bfa77
Maxime Bouchet <maxime.bouchet@epitech.eu>
2020-01-26 16:05:22 +0100

    opti

---
+++ tests/test_cpu_operand.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2020
+** test_operands
+** File description:
+** test method of class OPerands
+*/
+
+#include <criterion/criterion.h>
+#include "Memory.hh"
+#include "AbstractVmException.hh"
+#include "Output.hh"
+#include "Factory.hh"
+
